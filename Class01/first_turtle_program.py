79739858977ef2ffaf80c101d79f3448c71021b6
Tom Helmuth <thelmuth@hamilton.edu>
2021-02-02 10:03:44 -0500

    Code from 2/1/21

---
+++ Class01/first_turtle_program.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+leo = Turtle()
+shirley = Turtle()
+
+leo.speed(1)
+leo.forward(80)
+leo.width(3)
+
+leo.forward(50)
+leo.left(60)
+leo.forward(110)
+
+shirley.right(90)
+shirley.forward(100)
+
+leo.left(170)
+leo.forward(200)
+
