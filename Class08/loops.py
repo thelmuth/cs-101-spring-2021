0dcac9c85016acdeeba6722f5f47801b57731fa9
Tom Helmuth <thelmuth@hamilton.edu>
2021-02-17 20:20:19 -0500

    Code from 2/17/21

---
+++ Class08/loops.py
@@ -0,0 +1,34 @@
+
+# x = [1, 2, 3]
+# y = x
+# 
+# print(y)
+# 
+# y[0] = 500
+# 
+# print("y", y)
+# 
+# print("x", x)
+
+### What is going on here???
+### List aliasing
+
+
+####################
+## Loops stuff
+
+# print(0)
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+
+### A loop to print 0 to 5
+for number in range(1001):
+    print(number)
+print("The loop ended!")
+
+
+
