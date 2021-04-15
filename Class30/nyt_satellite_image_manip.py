6b6ae7bce834b7a3dccee85a21239d0410dcdafe
Tom Helmuth <thelmuth@hamilton.edu>
2021-04-15 19:59:38 -0400

    Fixed swap of code from classes 30 and 31

--- Class30/nyt_satellite_image_manip.py
+++ Class30/nyt_satellite_image_manip.py
@@ -10,18 +10,15 @@ from PIL import Image
 
 def main():
     ### This is a satellite image of Blue Ridge, VA
-#     satellite = Image.open("Blue_Ridge.jpg")
-#     satellite = Image.open("Nevada_Summerlin.jpg")
-    satellite = Image.open("Hamilton_College.jpg")
-    
-    satellite.show()
+    va = Image.open("Blue_Ridge.jpg")
+#     va.show()
     
     
-    print(satellite.width, satellite.height)
+    print(va.width, va.height)
 
 
     ## Sort the picture by luminance and put it back in order
-    sorted_image = nyt(satellite)
+    sorted_image = nyt(va)
     
     
     
@@ -36,56 +33,9 @@ def nyt(image):
     ### Want to sort the pixels by their luminance values
     ### How can we do that?
     
-    print("Starting to find luminance of pixels")
-    pixels_with_luminance = attach_luminance_to_pixels(pixels)
-    print("Done finding luminance of pixels")
+    pixels.sort()
     
-    print("The first 10 pixels with luminance are:", pixels_with_luminance[:10])
-    
-    pixels_with_luminance.sort()
-    
-    print("The first 10 pixels after sorting are:", pixels_with_luminance[:10])
-    
-    final_gradient = make_image_from_pixel_list(image.width, image.height,
-                                                pixels_with_luminance)
-    
-    final_gradient.show()
-    
-    
-def make_image_from_pixel_list(width, height, pixels_with_luminance):
-    """Creates a new image with given width and height, filled with the
-    pixels from pixels_with_luminance."""
-    
-    new_image = Image.new("RGB", (width, height))
-    
-    count = 0
-    
-    # Iterate through the locations in new_image
-    for y in range(height):
-        print("building new image at y =", y)
-        for x in range(width):
-            
-            pixel_with_luminance = pixels_with_luminance[count]
-            pixel = pixel_with_luminance[1]
-            
-            new_image.putpixel((x, y), pixel)
-        
-            count += 1
-            
-    return new_image
-
-    
-def attach_luminance_to_pixels(list_of_pixels):
-    """Take a list of pixels and make a list of tuples that have luminance
-    as the first element and the pixel as the second."""
-    
-    pixels_with_luminance = []
-    
-    for pixel in list_of_pixels:
-        pwl = (luminance(pixel), pixel)
-        pixels_with_luminance.append(pwl)
-        
-    return pixels_with_luminance
+    print("The first 10 pixels after sorting are:", pixels[:10])
     
     
     
--- Class31/nyt_satellite_image_manip.py
+++ Class31/nyt_satellite_image_manip.py
@@ -10,15 +10,18 @@ from PIL import Image
 
 def main():
     ### This is a satellite image of Blue Ridge, VA
-    va = Image.open("Blue_Ridge.jpg")
-#     va.show()
+#     satellite = Image.open("Blue_Ridge.jpg")
+#     satellite = Image.open("Nevada_Summerlin.jpg")
+    satellite = Image.open("Hamilton_College.jpg")
+    
+    satellite.show()
     
     
-    print(va.width, va.height)
+    print(satellite.width, satellite.height)
 
 
     ## Sort the picture by luminance and put it back in order
-    sorted_image = nyt(va)
+    sorted_image = nyt(satellite)
     
     
     
@@ -33,9 +36,56 @@ def nyt(image):
     ### Want to sort the pixels by their luminance values
     ### How can we do that?
     
-    pixels.sort()
+    print("Starting to find luminance of pixels")
+    pixels_with_luminance = attach_luminance_to_pixels(pixels)
+    print("Done finding luminance of pixels")
     
-    print("The first 10 pixels after sorting are:", pixels[:10])
+    print("The first 10 pixels with luminance are:", pixels_with_luminance[:10])
+    
+    pixels_with_luminance.sort()
+    
+    print("The first 10 pixels after sorting are:", pixels_with_luminance[:10])
+    
+    final_gradient = make_image_from_pixel_list(image.width, image.height,
+                                                pixels_with_luminance)
+    
+    final_gradient.show()
+    
+    
+def make_image_from_pixel_list(width, height, pixels_with_luminance):
+    """Creates a new image with given width and height, filled with the
+    pixels from pixels_with_luminance."""
+    
+    new_image = Image.new("RGB", (width, height))
+    
+    count = 0
+    
+    # Iterate through the locations in new_image
+    for y in range(height):
+        print("building new image at y =", y)
+        for x in range(width):
+            
+            pixel_with_luminance = pixels_with_luminance[count]
+            pixel = pixel_with_luminance[1]
+            
+            new_image.putpixel((x, y), pixel)
+        
+            count += 1
+            
+    return new_image
+
+    
+def attach_luminance_to_pixels(list_of_pixels):
+    """Take a list of pixels and make a list of tuples that have luminance
+    as the first element and the pixel as the second."""
+    
+    pixels_with_luminance = []
+    
+    for pixel in list_of_pixels:
+        pwl = (luminance(pixel), pixel)
+        pixels_with_luminance.append(pwl)
+        
+    return pixels_with_luminance
     
     
     
