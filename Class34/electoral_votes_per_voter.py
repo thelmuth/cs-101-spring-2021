c05e63e4c9f1e18567283e376ca93a62369049c5
Tom Helmuth <thelmuth@hamilton.edu>
2021-04-21 14:51:32 -0400

    Code from 4/21/21

---
+++ Class34/electoral_votes_per_voter.py
@@ -0,0 +1,64 @@
+"""
+Finds the number of electoral college votes per state, and then
+finds the population of each state, in order to compute the people per
+electoral college vote per state.
+
+Make a nice plot using matplotlib
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    electoral_votes = scrape_electoral_votes()
+    
+    print(electoral_votes)
+    
+
+def scrape_electoral_votes():
+    """Returns a dictionary of the electoral college votes for each state."""
+    
+    url = "https://www.britannica.com/topic/United-States-Electoral-College-Votes-by-State-1787124"
+    
+    ### get returns the data from the url.
+    ### .text gives us the actual source code
+    source_code = requests.get(url).text
+    
+    ### creates a BeautifulSoup object, which stores all of the data about the HTML
+    html = BeautifulSoup(source_code, "html.parser")
+
+    electoral_votes = {}
+    
+    ### This finds the _first_ table in the page
+    table = html.find("table")
+    
+    ### Find all table rows in the source code
+    ### This returns a list of new BeautifulSoup objects, so we can look through the
+    ### list and find other things within it.
+    rows = table.find_all("tr")
+
+    ### Iterate through the list of table rows:
+    for row in rows:
+        
+        ## Find all td elements within a row
+        cells = row.find_all("td")
+        
+        # Make sure row has some td cells
+        if cells != []:
+#             print(cells[0].text)
+#             print(cells[1].text)
+#             print(cells[2].text)
+#             print(cells[3].text)
+        
+            ## Fill our dictionary with the data we've scraped
+            electoral_votes[cells[0].text] = int(cells[1].text)
+            
+            if cells[2].text != "":
+                electoral_votes[cells[2].text] = int(cells[3].text)
+        
+
+    return electoral_votes
+        
+
+
+main()
